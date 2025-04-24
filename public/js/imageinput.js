908300719d2fc0453024bef78e2a766e5373988e
devjuju <jleleubellpro1994@gmail.com>
2025-04-24 16:29:37 +0200

    Responsive desktop terminé

--- public/js/imageinput.js
+++
@@ -1,6 +0,0 @@
-
-var imageInputElement = document.querySelector("#kt_image_input_example_1");
-var imageInput = KTImageInput.getInstance(imageInputElement);
-imageInput.on("kt.imageinput.change", function() {
-    // console.log("kt.imageinput.change event is fired");
-});
